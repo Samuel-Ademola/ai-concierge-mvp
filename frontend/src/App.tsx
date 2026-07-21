d84e18350b6d38c56b18c07bcc6ccef1cb27bd48
Samuel-Ademola <Samademoladayo@gmail.com>
2026-07-21 12:31:34 +0100

    feat: implement guest preferences form from vague prompt

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+﻿import GuestPreferencesForm from "./components/GuestPreferencesForm";
+
+export default function App() {
+  return (
+    <div className="App">
+      <GuestPreferencesForm />
+    </div>
+  );
+}
